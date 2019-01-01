862836f8b4394158e568d4d9da04b3eaa9f11049
A Glazer <avigayilglazer@gmail.com>
2019-01-01 18:57:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,11 +9,11 @@ def batch_badge_creator(array)
     array.collect {|attendees| "Hello, my name is #{attendees}."}
 end
 
-def assign_rooms(speakers)
-  speakers.map.with_index(1) {|name, num| "Hello, #{name}! You'll be assigned to room #{num}!"}
+def assign_rooms(array)
+  array.map.with_index(1) {|name, num| "Hello, #{name}! You'll be assigned to room #{num}!"}
 end
 
-def printer(name)
-   name.each {|name| puts "Hello, my name is #{name}."}
-   name.map.with_index(1) {|name, num| puts "Hello, #{name}! You'll be assigned to room #{num}!"}
+def printer(array)
+  batch_badge_creator(array).each
+  assign_rooms(speakers).each
 end
